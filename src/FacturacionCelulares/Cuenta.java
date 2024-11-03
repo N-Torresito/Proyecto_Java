11bea19b39a8b442cb93193a4d7425281f0cc22e
Torresito <178304045+N-Torresito@users.noreply.github.com>
2024-11-03 16:58:40 -0500

    Clase Prepago: Añadido manera métodos para recarga, ya sea con fecha o en la fecha actual. Clase Postpago: añadido cargo fijo de 20000. Clase Cuenta: Añadidas variables para objectos Postpago/Prepago, modificado constructor para elegir el tipo de cuenta y añadidos getters y setters

--- src/FacturacionCelulares/Cuenta.java
+++ src/FacturacionCelulares/Cuenta.java
@@ -1,22 +1,120 @@
+// Paquete de facturación de celulares
 package FacturacionCelulares;
 
+// Importación de librerías
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * Clase Cuenta
+ */
 public class Cuenta implements Serializable {
     private long SerialVersionUID = 1L;
     private long id;
     private long numero;
+    private Prepago tipoPrepago = null;
+    private Postpago tipoPostpago = null;
     private List<Llamada> llamadas;
 
-    public Cuenta(long id, long numero) {
+    /**
+     * Constructor de la clase Cuenta
+     * @param id
+     * @param numero
+     * @param tipo
+     */
+    public Cuenta(long id, long numero, String tipo) {
         this.id = id;
         this.numero = numero;
         llamadas = new ArrayList<Llamada>();
+        if (tipo.equals("Prepago")) {
+            tipoPrepago = new Prepago();
+        } else if (tipo.equals("Postpago")) {
+            tipoPostpago = new Postpago();
+        }
     }
+
     public long obtenerPagoCuenta(){
         return 1L;
         //TODO - Create method / Crear método
     }
 
+    /**
+     * Método para devolver la lista de llamadas
+     * @return
+     */
+    public List<Llamada> getLlamadas() {
+        return llamadas;
+    }
+
+    /**
+     * Método para modificar la lista de llamadas
+     * @param llamadas
+     */
+    public void setLlamadas(List<Llamada> llamadas) {
+        this.llamadas = llamadas;
+    }
+
+    /**
+     * Método para devolver el tipo de postpago
+     * @return
+     */
+    public Postpago getTipoPostpago() {
+        return tipoPostpago;
+    }
+
+    /**
+     * Método para modificar el tipo de postpago
+     * @param tipoPostpago
+     */
+    public void setTipoPostpago(Postpago tipoPostpago) {
+        this.tipoPostpago = tipoPostpago;
+    }
+
+    /**
+     * Método para devolver el tipo de prepago
+     * @return
+     */
+    public Prepago getTipoPrepago() {
+        return tipoPrepago;
+    }
+
+    /**
+     * Método para modificar el tipo de prepago
+     * @param tipoPrepago
+     */
+    public void setTipoPrepago(Prepago tipoPrepago) {
+        this.tipoPrepago = tipoPrepago;
+    }
+
+    /**
+     * Método para devolver el número de la cuenta
+     * @return
+     */
+    public long getNumero() {
+        return numero;
+    }
+
+    /**
+     * Método para modificar el número de la cuenta
+     * @param numero
+     */
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * Método para devolver el id de la cuenta
+     * @return
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Método para modificar el id de la cuenta
+     * @param id
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
 }
--- src/FacturacionCelulares/Prepago.java
+++ src/FacturacionCelulares/Prepago.java
@@ -1,21 +1,21 @@
 package FacturacionCelulares;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Prepago implements Serializable {
-    private long SerialVersionUID = 1L;
-    public long numeroMinutos;
-    public List<Recarga> recargas;
+    private static final long serialVersionUID = 1L;
+    private long numeroMinutos;
+    private List<Recarga> recargas;
 
     /**
-     * Constructor de la clase Prepago con parametros
-     * @param numeroMinutos - Cantidad de minutos
+     * Constructor de la clase SetMinutos
      */
-    public Prepago(long numeroMinutos) {
-        this.numeroMinutos = numeroMinutos;
-        recargas = new ArrayList<>();
+    public void SetMinutos() {
+        this.numeroMinutos = 5;
+        recargas = new ArrayList<Recarga>();
     }
 
     /**
@@ -27,11 +27,22 @@ public class Prepago implements Serializable {
     }
 
     /**
-     * Método para modificar la cantidad de minutos
+     * Método para modificar la cantidad de minutos con fecha
+     * @param fecha
      * @param numeroMinutos
      */
-    public void setNumeroMinutos(long numeroMinutos) {
-        this.numeroMinutos = numeroMinutos;
+    public void recargaConFecha(LocalDate fecha, long numeroMinutos) {
+        this.numeroMinutos += numeroMinutos;
+        recargas.add(new Recarga(fecha,numeroMinutos));
+    }
+
+    /**
+     * Método para realizar una recarga
+     * @param numeroMinutos
+     */
+    public void Recarga(long numeroMinutos) {
+        this.numeroMinutos += numeroMinutos;
+        recargas.add(new Recarga(LocalDate.now(),numeroMinutos));
     }
 
     /**
@@ -48,6 +59,18 @@ public class Prepago implements Serializable {
      */
     public void setRecargas(List<Recarga> recargas) {
         this.recargas = recargas;
+    }
+
+    /**
+     * Método para devolver la información de la recarga
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "Prepago{" +
+                "Número minutos=" + numeroMinutos +
+                ",\n recargas:\n" + recargas +
+                '}';
     }
 }
 
