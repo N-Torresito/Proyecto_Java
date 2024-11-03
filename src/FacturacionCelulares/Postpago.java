7c75aaadb3233fe974784b139f1e417142c504c0
Torresito <178304045+N-Torresito@users.noreply.github.com>
2024-11-03 16:58:40 -0500

    Clase Prepago: Añadido manera métodos para recarga, ya sea con fecha o en la fecha actual. Clase Postpago: añadido cargo fijo de 20000. Clase Cuenta: Añadidas variables para objectos Postpago/Prepago, modificado constructor para elegir el tipo de cuenta y añadidos getters y setters

--- src/FacturacionCelulares/Postpago.java
+++ src/FacturacionCelulares/Postpago.java
@@ -6,20 +6,35 @@ public class Postpago implements Serializable {
     private long SerialVersionUID = 1L;
     private long cargoFijo;
 
-    public Postpago(long cargoFijo) {
-        this.cargoFijo = cargoFijo;
+    /**
+     * Constructor de la clase Postpago
+     */
+    public Postpago() {
+        this.cargoFijo = 20000;
     }
 
+    /**
+     * Método para devolver el cargo fijo
+     * @return cargo fijo
+     */
     public long getCargoFijo() {
         return cargoFijo;
     }
 
+    /**
+     * Método para modificar el cargo fijo
+     * @param cargoFijo
+     */
     public void setCargoFijo(long cargoFijo) {
         this.cargoFijo = cargoFijo;
     }
 
+    /**
+     * Sobreescritura del método toString heredado
+     * @return
+     */
     @Override
     public String toString() {
-        return "Postpago{" + "cargoFijo=" + cargoFijo + '}';
+        return "Postpago{" + "Cargo fijo:" + cargoFijo + '}';
     }
 }
